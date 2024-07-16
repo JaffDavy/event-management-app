c3f73083aa332c5fc961143f8c2e2dee7906f35c
temchelsy <148960005+temchelsy@users.noreply.github.com>
2024-07-16 15:39:52 +0100

    register/login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import EventForm from './EventForm'
+import React, { useState } from 'react';
+import EventForm from './EventForm';
+import { Login } from './Login';
+import { Register } from './Register';
+
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
 
   return (
-    <>
+    <div>
       <h1>Event Management System</h1>
-      < EventForm  />
-    </>
-  )
+      <EventForm />
+      <div className="App">
+        {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
