294db6812aa77d76bd7ed7824aa6f46f198df741
JaffDavy <jaffdavy100@gmail.com>
2024-07-16 17:23:05 +0100

    inserted navbar home page picture and iconwrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import NavBar from './Components/Navbar'
+import Iconwrapper from './Components/Iconwrapper'
 
 function App() {
 
   return (
     <>
-      <h1>Event Management System</h1>
+      <header>
+        <NavBar />
+      </header>
+      <body>
+        <div className='fineyourmatch'>
+          <button><a href='https://www.eventbrite.com/b/local/home-and-lifestyle/dating/' class="button-link">Fine your next date</a></button>
+        </div>
+        <Iconwrapper />
+      </body>
     </>
   )
 }
---
+++ src/Components/Iconwrapper.jsx
@@ -0,0 +1,59 @@
+function Iconwrapper() {
+  return (
+    <div className='iconCategoryBrowser'>
+      <div className='iconCategoryWrapper'>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/music" className="icon-button">
+            <i class="fa-solid fa-microphone"></i>
+          </a>
+          <p>Music</p>
+        </div>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/nightlife" className="icon-button">
+            <i class="fa-solid fa-microphone"></i>
+          </a>
+          <p>Nightlife</p>
+        </div>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/nightlife" className="icon-button">
+            <i class="fa-solid fa-microphone"></i>
+          </a>
+          <p>Performing & Visual Arts</p>
+        </div>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/nightlife" className="icon-button">
+            <i class="fa-solid fa-microphone"></i>
+          </a>
+          <p>Holidays</p>
+        </div>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/nightlife" className="icon-button">
+            <i class="fa-solid fa-microphone"></i>
+          </a>
+          <p>Dating</p>
+        </div>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/nightlife" className="icon-button">
+            <i class="stadia_controller"></i>
+            <p>Hobbies</p>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/nightlife" className="icon-button">
+            <i class="fa-solid fa-microphone"></i>
+          </a>
+          <p>Business</p>
+        </div>
+        <div>
+          <a href="https://www.eventbrite.com/b/local/nightlife" className="icon-button">
+            <i class="fa-solid fa-microphone"></i>
+          </a>
+          <p>Food & Drinks</p>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Iconwrapper;
