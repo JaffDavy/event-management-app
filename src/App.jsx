78820ca64b60dd4fa2fabc1cdaeae1fba68627fc
JaffDavy <jaffdavy100@gmail.com>
2024-07-16 17:23:05 +0100

    inserted navbar home page picture and iconwrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import EventForm from './EventForm';
 import { Login } from './Login';
 import { Register } from './Register';
+import './App.css'; // Import your CSS file here
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -17,6 +18,15 @@ function App() {
       <div className="App">
         {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
       </div>
+      <header>
+        {/* Include your NavBar component here */}
+      </header>
+      <body>
+        <div className='fineyourmatch'>
+          <button><a href='https://www.eventbrite.com/b/local/home-and-lifestyle/dating/' className="button-link">Find your next date</a></button>
+        </div>
+        {/* Include your Iconwrapper component here */}
+      </body>
     </div>
   );
 }
