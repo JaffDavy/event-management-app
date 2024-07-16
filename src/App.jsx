ff2a6d1e68646509665eb648a950bc1f441d352d
JaffDavy <jaffdavy100@gmail.com>
2024-07-16 17:23:05 +0100

    inserted navbar home page picture and iconwrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 // import logo from './logo.svg';
-import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
+import './App.css'
+import NavBar from './Components/Navbar'
+import Iconwrapper from './Components/Iconwrapper'
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -18,6 +20,18 @@ function App() {
       }
     </div>
   );
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <body>
+        <div className='fineyourmatch'>
+          <button><a href='https://www.eventbrite.com/b/local/home-and-lifestyle/dating/' class="button-link">Fine your next date</a></button>
+        </div>
+        <Iconwrapper />
+      </body>
+    </>
+  )
 }
 
 export default App;
