e9e3bff6e607bd5eca93cd9cee9b136f1c86d74d
JaffDavy <jaffdavy100@gmail.com>
2024-07-23 10:00:57 +0100

    completed home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import './App.css'
 import NavBar from './Components/Navbar'
 import Iconwrapper from './Components/Iconwrapper'
 import Trend from './Components/Toptrendberlin'
+import Moreevents from './Components/moreevents'
+import Popular from './Components/popularcitys'
+import About from './Components/About'
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -31,6 +34,9 @@ function App() {
         </div>
         <Iconwrapper />
         <Trend />
+        <Moreevents />
+        <Popular />
+        <About />
       </body>
     </>
   )
