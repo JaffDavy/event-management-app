b96d15c0d509f17f8c430aa900b847eaea6a60e9
JaffDavy <jaffdavy100@gmail.com>
2024-07-24 09:47:50 +0100

    full home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import { Login } from "./Login";
 import { Register } from "./Register";
-import './App.css'
-import NavBar from './Components/Navbar'
-import Iconwrapper from './Components/Iconwrapper'
-import Trend from './Components/Toptrendberlin'
-import Moreevents from './Components/moreevents'
-import Popular from './Components/popularcitys'
-import About from './Components/About'
+import Homepage from "./Components/Home-page";
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -19,18 +13,8 @@ function App() {
   return (
 
   <>
-    <header>
-      <NavBar />
-    </header>
     <body>
-      <div className='fineyourmatch'>
-        <button><a href='https://www.eventbrite.com/b/local/home-and-lifestyle/dating/' class="button-link">Fine your next date</a></button>
-      </div>
-      <Iconwrapper />
-      <Trend />
-      <Moreevents />
-      <Popular />
-      <About />
+      <Homepage />  
     </body>
     <div className="App">
       {
