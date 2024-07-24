fd6dbcae5dce01ba68bce7f9b55e509190c63542
JaffDavy <jaffdavy100@gmail.com>
2024-07-24 17:04:49 +0100

    implemented title, data ,time and location for creation of events

---
+++ src/Components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+
+    return (
+        <>
+            <div id="head">
+                <div><h2>D&C Event App</h2></div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/Components/titlesummary.jsx
@@ -0,0 +1,23 @@
+
+const Tittesummary = () => {
+
+    return (
+        <>
+            <div class="card">
+                <h2>Event Overview</h2>
+                <h4>Event title</h4>
+                <form>
+                    <label for="event-title">Be clear and descriptive with a title that tells people what your event is about.</label>
+                    <input type="text" id="event-title" name="event-title" required />
+                    <h4>Event Summary</h4>
+                    <label for="event-summary">Grab people's attention with a short description about your event. Attendees will see this at the top of your event page. (140 characters max)</label>
+                    <textarea id="event-summary" name="event-summary" rows="4" required></textarea>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default Tittesummary;
