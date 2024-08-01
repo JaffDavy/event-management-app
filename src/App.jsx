e6e9a43252110c795ef13f959830c2fbc70ccc8a
JaffDavy <jaffdavy100@gmail.com>
2024-08-01 09:50:09 +0100

    Events form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
-import EventForm from './Components/EventForm'
+// import EventForm from './Components/EventForm'
 // import Eventpage from './Components/Eventpage'
 // import Header from './Components/header'
+import Dashboard from './Components/dashboard'
 
 function App() {
 
   return (
     <>
-      {/* <Header />
-      <Eventpage /> */}
-      < EventForm  />
+      {/* <Header /> */}
+      {/* <Eventpage />  */}
+      {/* < EventForm  /> */}
+      <Dashboard />
     </>
   )
 }
