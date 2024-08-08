821c09e7349cdc52d4125b3ff2367a87756b0158
JaffDavy <jaffdavy100@gmail.com>
2024-08-08 14:23:08 +0100

    merging create event page with dev-branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css'
-// import EventForm from './Components/EventForm'
-// import Eventpage from './Components/Eventpage'
-// import Header from './Components/header'
-import Dashboard from './Components/dashboard'
+import CreateEvent from './Components/pages/CreateEvent'
 
 function App() {
 
   return (
     <>
-      {/* <Header /> */}
-      {/* <Eventpage />  */}
-      {/* < EventForm  /> */}
-      <Dashboard />
+      <CreateEvent />
     </>
   )
 }
