0d36360ba053e4bd9a7a70470fe583797279dab6
JaffDavy <jaffdavy100@gmail.com>
2024-08-09 11:00:49 +0100

    merging create event page with home

---
+++ frontend/src/Component/pages/create-event/CreateEvent.jsx
@@ -0,0 +1,19 @@
+import "./create-event.css";
+import EventForm from '../../EventForm'
+import Eventpage from '../../Eventpage'
+import Header from '../../header'
+import Dashboard from '../../dashboard'
+
+function CreateEvent() {
+
+  return (
+    <>
+      <Header />
+      <Eventpage /> 
+      < EventForm  />
+      <Dashboard />
+    </>
+  )
+}
+
+export default CreateEvent
