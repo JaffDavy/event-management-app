44052ea82548af752868f7b531f7cd7588c92484
temchelsy <148960005+temchelsy@users.noreply.github.com>
2024-08-30 15:20:59 +0100

    solving conflicts

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,21 +1,22 @@
 import pg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // This is usually required for cloud-hosted databases.
-  },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool
   .connect()
   .then(() => {
-    console.log("Connected to the remote database");
+    console.log("Connected to the database on port 5432")
+   
   })
-  .catch((err) => console.error("Error connecting to the database:", err));
+  .catch((err) => console.error(err));
 
 export default pool;
